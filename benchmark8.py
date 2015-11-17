f1c19d55da8e90e48e401ef276fd0182450a558d
zborowski <zborowski@antmicro.com>
2015-11-17 14:57:52 +0100

    Added teest for file download using wget command.

--- benchmark8.py
+++ benchmark8.py
@@ -5,8 +5,17 @@ import socket
 import time
 import requests
 
+try:
+    # for python 3
+    from subprocess import DEVNULL
+except ImportError:
+    # for python 2
+    import os
 
-def download_file():
+    DEVNULL = open(os.devnull, 'wb')
+
+
+def download_file1():
     downloaded_bytes = 0
     chunk_size = 1024 * 8
 
@@ -30,14 +39,10 @@ def download_file():
 
             with open(file_name, 'wb') as fd:
                 for chunk in response.iter_content(chunk_size=chunk_size):
-
                     # Filter out keep-alive new chunks
                     if chunk:
                         fd.write(chunk)
-
-                        downloaded_bytes += chunk_size
-                        download_progress = (downloaded_bytes * 100) / file_size
-                        # print('Downloaded: %d%s' % (download_progress, '%'))
+                        downloaded_bytes += len(chunk)
 
     except requests.exceptions.ConnectionError:
         print('Cannot download file from server: %s, connection error' % server)
@@ -49,21 +54,45 @@ def download_file():
         print('Cannot download file from server: %s, connection error' % server)
 
 
-if __name__ == '__main__':
-    server = 'http://developer.toradex.com/files/toradex-dev/uploads/media/Colibri/Linux/Images/'
-    file_name = 'Apalis_T30_LinuxImageV2.5Beta2_20151106.tar.bz2'
+def download_file2():
+    try:
+        subprocess.check_call(['wget', '/'.join([server, file_name])], shell=False, stdout=DEVNULL, stderr=DEVNULL)
 
-    # Remove old downloaded file
+    except subprocess.CalledProcessError, e:
+        print('Download file, error: %s' % e.__str__())
+
+
+def remove_file():
     try:
         subprocess.check_call('rm -f ' + file_name, shell=True)
 
     except subprocess.CalledProcessError:
         print('Failed to clean existing file copy')
 
-    start_time = time.time()
 
-    download_file()
+if __name__ == '__main__':
+    server = 'http://developer.toradex.com/files/toradex-dev/uploads/media/Colibri/Linux/Images/'
+    file_name = 'Apalis_T30_LinuxImageV2.5Beta2_20151106.tar.bz2'
 
-    end_time = time.time()
+    # Remove old downloaded file
+    remove_file()
 
-    print ("Download time: %f" % (end_time - start_time))
+    start_time1 = time.time()
+
+    # Legacy method
+    download_file1()
+
+    legacy_download_time = time.time()
+
+    # Remove old downloaded file
+    remove_file()
+
+    start_time2 = time.time()
+
+    # WGET method
+    download_file2()
+
+    wget_download_time = time.time()
+
+    print ("Download time using legacy method: %f" % (legacy_download_time - start_time1))
+    print ("Download time using wget method: %f" % (wget_download_time - start_time2))
