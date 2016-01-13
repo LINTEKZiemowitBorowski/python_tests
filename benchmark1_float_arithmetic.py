99ee10b4af6fed85c41d534ae37cae2b2a2b5878
zborowski <zborowski@antmicro.com>
2016-01-13 15:17:01 +0100

    Two ways of running tasks: using threading and using multiprocessing.Pool

--- benchmark1_float_arithmetic.py
+++ benchmark1_float_arithmetic.py
@@ -5,27 +5,44 @@ import threading
 import time
 import math
 
+from multiprocessing import Pool
 
-class Worker(threading.Thread):
+NUM_ITERATIONS = 1000000
+NUM_TASKS = 50
 
-    def run(self):
-        result = 0
-        for x in xrange(1000000):
-            # print("Iteration: %d" % x)
-            p = x * math.pi
-            result += math.sqrt(x**2 + p**2)
 
-        return result
+def my_function1(in_value, out_value):
+    out_value[in_value] = sum([math.sqrt((z * z) + (z * z * math.pi * math.pi)) + in_value
+                               for z in xrange(NUM_ITERATIONS)])
+
+
+def my_function2(in_value):
+    return sum([(math.sqrt(z * z) + (z * z * math.pi * math.pi)) + in_value
+                for z in xrange(NUM_ITERATIONS)])
 
 
 if __name__ == '__main__':
     print ("Running: %s" % os.path.basename(__file__))
 
-    jobs = [Worker() for i in xrange(50)]
-    [t.start() for t in jobs]
+    results = {}
+    jobs = [threading.Thread(target=my_function1, kwargs=dict(in_value=i, out_value=results))
+            for i in xrange(NUM_TASKS)]
     start_time = time.time()
+    [t.start() for t in jobs]
     [t.join() for t in jobs]
     end_time = time.time()
     execution_time = end_time - start_time
 
-    print ("Execution time: %f\n" % execution_time)
+    print ("Execution time(using threading): %f\n" % execution_time)
+    # print ("Results: %s\n" % results.values())
+
+    pool = Pool(processes=4)
+    start_time = time.time()
+    results = [pool.apply_async(my_function2, [p]) for p in xrange(NUM_TASKS)]
+    pool.close()
+    pool.join()
+    end_time = time.time()
+    execution_time = end_time - start_time
+
+    print ("Execution time(using multiprocessing.Pool): %f" % execution_time)
+    # print ("Results: %s\n" % [x.get() for x in results])
--- benchmark2_int_arithmetic.py
+++ benchmark2_int_arithmetic.py
@@ -4,27 +4,44 @@ import os
 import threading
 import time
 
+from multiprocessing import Pool
 
-class Worker(threading.Thread):
+NUM_ITERATIONS = 1000000
+NUM_TASKS = 50
 
-    def run(self):
-        result = 0
-        for x in xrange(1000000):
-            # print("Iteration: %d" % x)
-            p = x * 3
-            result += ((x*x) + (p*p)) % 5
 
-        return result
+def my_function1(in_value, out_value):
+    out_value[in_value] = sum([(((z * z) + (z * z * 9)) / 5) + in_value
+                               for z in xrange(NUM_ITERATIONS)])
+
+
+def my_function2(in_value):
+    return sum([(((z * z) + (z * z * 9)) / 5) + in_value
+                for z in xrange(NUM_ITERATIONS)])
 
 
 if __name__ == '__main__':
-    print ("Running: %s" % os.path.basename(__file__))\
+    print ("Running: %s" % os.path.basename(__file__))
 
-    jobs = [Worker() for i in xrange(50)]
-    [t.start() for t in jobs]
+    results = {}
+    jobs = [threading.Thread(target=my_function1, kwargs=dict(in_value=i, out_value=results))
+            for i in xrange(NUM_TASKS)]
     start_time = time.time()
+    [t.start() for t in jobs]
     [t.join() for t in jobs]
     end_time = time.time()
     execution_time = end_time - start_time
 
-    print ("Execution time: %f\n" % execution_time)
+    print ("Execution time(using threading): %f\n" % execution_time)
+    # print ("Results: %s\n" % results.values())
+
+    pool = Pool(processes=4)
+    start_time = time.time()
+    results = [pool.apply_async(my_function2, [p]) for p in xrange(NUM_TASKS)]
+    pool.close()
+    pool.join()
+    end_time = time.time()
+    execution_time = end_time - start_time
+
+    print ("Execution time(using multiprocessing.Pool): %f" % execution_time)
+    # print ("Results: %s\n" % [x.get() for x in results])
